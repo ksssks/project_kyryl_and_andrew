1a9a6d5121a1b98a7e6c1b8fb25b64d4a3f1b3e1
unknown <kyrylshulga.in@gmail.com>
2024-11-12 10:33:21 +0200

    style changes in orders

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -50,19 +50,19 @@ const Orders = ({ token }) => {
 
   return (
     <div>
-      <h3>Замовлення</h3>
+      <h3 className='dark:text-white'>Замовлення</h3>
       <div>
         {
           orders.map((order, index) => (
-            <div key={index}>
-              <img src={assets.parcel_icon} alt="" />
+            <div className='grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700 dark:border-white dark:text-white rounded-xl' key={index}>
+              <img src={assets.parcel_icon} className='w-12 dark:invert dark:bg-white' alt="" />
               <div>
                 <div>
                   {order.items.map((item, index) => {
-                    return <p key={index}> {item.name} x {item.quantity} <span> {item.size} </span></p>
+                    return <p className='py-0.5' key={index}> {item.name} x {item.quantity} <span> {item.size} </span></p>
                   })}
                 </div>
-                <p>{order.address.firstName + ' ' + order.address.lastName}</p>
+                <p className='mt-3 mb-2 font-medium'>{order.address.firstName + ' ' + order.address.lastName}</p>
                 <div>
                   <p>{order.address.street + ','}</p>
                   <p>{order.address.country + ', ' + order.address.city + ' ' + order.address.state}</p>
@@ -71,13 +71,13 @@ const Orders = ({ token }) => {
                 <p>{order.address.phone}</p>
               </div>
               <div>
-                <p>Найменувань товарів: {order.items.length}</p>
-                <p>Спосіб оплати: {order.paymentMethod}</p>
+                <p className='text-sm sm:text-[15px]'>Найменувань товарів: {order.items.length}</p>
+                <p className='mt-3'>Спосіб оплати: {order.paymentMethod}</p>
                 <p>{order.payment ? 'Замовлення оплачено' : 'Очікуємо на оплату'}</p>
                 <p>Дата замовлення: {new Date(order.date).toLocaleDateString('uk-UA', { weekday: 'short', day: '2-digit', month: 'short', year: 'numeric' })}</p>
               </div>
-              <p> {order.amount}{currency}</p>
-              <select onChange={(event) => statusHandler(event, order._id)} value={order.status}>
+              <p className='text-sm sm:text-[15px]'> {order.amount}{currency}</p>
+              <select className='p-2 py-3 font-semibold dark:text-black' onChange={(event) => statusHandler(event, order._id)} value={order.status}>
                 <option value="Замовлення оформлено">Замовлення оформлено</option>
                 <option value="Пакування замовлення">Пакування замовлення</option>
                 <option value="Готово до відправлення">Готово до відправлення</option>
